3dd1658994ccb5597ddfbd1edb09f612f1418fe6
Bronte Adshead <bronte.adshead@43379.auckland.natcoll.ac.nz>
2014-07-07 15:20:50 +1200

    Changed JS

--- main.js
+++ main.js
@@ -5,21 +5,34 @@ var game = new Phaser.Game(400, 490, Phaser.AUTO, 'game_div');
 var main_state = {
 
     preload: function() { 
-		// Function called first to load all the assets
-		this.game.stage.backgroundColor = '#66ccff';
+        // Function called first to load all the assets
+        this.game.stage.backgroundColor = '#66ccff';
 
-		this.game.load.image('bird', 'assets/bird.png')
+        this.game.load.image('bird', 'assets/bird.png')
     },
 
     create: function() { 
-    	// Fuction called after 'preload' to setup the game
-    	this.bird = this.game.add.sprite(100, 245, 'bird');
+        // Fuction called after 'preload' to setup the game
+        this.bird = this.game.add.sprite(100, 245, 'bird');
 
-    	this.bird.body.gravity.y = 1000; 
+        this.bird.body.gravity.y = 1000; 
+
+        var space_key = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+        space_key.onDown.add(this.jump, this);
     },
     
     update: function() {
-		// Function called 60 times per second
+        // Function called 60 times per second
+        if (this.bird.inWorld == false)
+            this.restart_game();
+    },
+
+    jump: function() {
+        this.bird.velocity.y = -350;
+    },
+
+    restart_game: function() {
+        this.game.state.start('main');
     },
 };
 
