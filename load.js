94b77c68e8a095f2dcc507525e9082da58afe7a0
bronte-a <bronte_a@hotmail.co.nz>
2014-07-09 15:17:40 +1200

    Update load.js

--- load.js
+++ load.js
@@ -6,7 +6,8 @@ var load_state = {
         this.game.load.image('bird', 'assets/donut.png');  
         this.game.load.image('pipe', 'assets/pipe.png');  
         this.game.load.audio('jump', 'assets/jump.wav');
-    },
+        this.game.load.audio('jump', 'assets/jump.wav');
+    }
 
     create: function() {
         // When all assets are loaded, go to the 'menu' state
